f025c7c406a266a9a05d647f61a93102fa06b38d
Duncan Mackenzie <duncanma@microsoft.com>
2018-12-05 12:53:10 -0800

    Adding a custom rule to require all links to Microsoft properties to be secure (https vs http)

---
+++ docs-markdown/markdownlint-custom-rules/securelinks.js
@@ -0,0 +1,49 @@
+// @ts-check
+
+"use strict";
+
+const common = require("./common");
+const detailStrings = require("./strings");
+
+module.exports = {
+  "names": ["secure-links"],
+  "description": "All links to Microsoft properties should be secure",
+  "tags": ["compliance"],
+  "function": function rule(params, onError) {
+    params.tokens.filter(function filterToken(token) {
+      return token.type === "inline";
+    }).forEach(function forToken(inline) {
+      inline.children.filter(function filterChild(child) {
+        return child.type === "link_open";
+      }).forEach(function forToken(link) {
+        if (link.attrs[0] && link.attrs[0][0] == "href") {
+          const linkPattern = /^(http:\/\/www\.|http:\/\/)([^\/]*)\/.*$/;
+
+          const href = link.attrs[0][1];
+          if (href.startsWith("http://")) {
+            let match = href.match(linkPattern);
+            let domain = "";
+            if (match && match.length === 3) {
+              domain = match[2];
+              if (domain.includes("microsoft.com")
+                || domain.includes("azure.com")
+                || domain.includes("visualstudio.com")
+                || domain.includes("msdn.com")) {
+                let range = null;
+                let column = link.line.indexOf(href);
+                let length = href.length;
+                range = [column, length];
+                onError({
+                  "lineNumber": link.lineNumber,
+                  "detail": "Link " + href,
+                  "context": link.line,
+                  "range": range
+                });
+              }
+            }
+          }
+        }
+      });
+    });
+  }
+};
---
+++ docs-markdown/test/markdownlint-custom-rules/securelinks.spec.js
@@ -0,0 +1,30 @@
+const markdownlint = require('markdownlint');
+const securelinks = require('../../markdownlint-custom-rules/securelinks');
+const errorDetailStrings = require("../../markdownlint-custom-rules/strings");
+
+test('validate extension', () => {
+    const src = `${__dirname}/test-content/securelinks.md`;
+    const results = markdownlint.sync({
+        customRules: securelinks,
+        files: [src],
+    });
+
+    expect(results[src]).toEqual([
+        {
+            "errorContext": "[Microsoft Home Content](http://www.microsoft.com/foo/test.txt)",
+            "errorDetail": "Link http://www.microsoft.com/foo/test.txt",
+            "errorRange": [25, 37],
+            "lineNumber": 5,
+            "ruleDescription": "All links to Microsoft properties should be secure",
+            "ruleNames": ["secure-links"]
+        },
+        {
+            "errorContext": "[Go get Visual Studio](http://www.visualstudio.com/foo/test)",
+            "errorDetail": "Link http://www.visualstudio.com/foo/test",
+            "errorRange": [23, 36],
+            "lineNumber": 17,
+            "ruleDescription": "All links to Microsoft properties should be secure",
+            "ruleNames": ["secure-links"]
+        }
+    ]);
+});
