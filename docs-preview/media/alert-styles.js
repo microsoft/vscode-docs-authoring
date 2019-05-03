c9dd9f4f2747b900d7620fd9d2ff94d776b356e4
Jamar Warren <jamarw@users.noreply.github.com>
2019-05-03 16:05:57 -0700

    Update alert-styles.js

--- docs-preview/media/alert-styles.js
+++ docs-preview/media/alert-styles.js
@@ -1,34 +1,14 @@
 var blockquoteTags = document.getElementsByTagName("blockquote");
+var alerts = ["NOTE", "TIP", "IMPORTANT", "CAUTION", "WARNING"];
 
-for (var i = 0; i < blockquoteTags.length; i++) {
-  if (blockquoteTags[i].innerHTML.indexOf("[!NOTE]") > -1) {
-    blockquoteTags[i].setAttribute("class", "NOTE");
-    var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!NOTE]", "<strong>Note</strong><br>");
-    blockquoteTags[i].innerHTML = addBreak;
+alerts.map(aa => {
+  for (var i = 0; i < blockquoteTags.length; i++) {
+    let tag = blockquoteTags[i];
+    if (tag.innerHTML.toUpperCase().indexOf(`[!${aa}]`) > -1) {
+      tag.setAttribute("class", aa);
+      var alertType = blockquoteTags[i].innerHTML;
+      var addBreak = alertType.replace(`[!${aa}]`, `<strong>${aa}</strong><br>`);
+      blockquoteTags[i].innerHTML = addBreak;
+    }
   }
-  if (blockquoteTags[i].innerHTML.indexOf("[!TIP]") > -1) {
-    blockquoteTags[i].setAttribute("class", "TIP");
-    var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!TIP]", "<strong>Tip</strong><br>");
-    blockquoteTags[i].innerHTML = addBreak;
-  }
-  if (blockquoteTags[i].innerHTML.indexOf("[!IMPORTANT]") > -1) {
-    blockquoteTags[i].setAttribute("class", "IMPORTANT");
-    var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!IMPORTANT]", "<strong>Important</strong><br>");
-    blockquoteTags[i].innerHTML = addBreak;
-  }
-  if (blockquoteTags[i].innerHTML.indexOf("[!CAUTION]") > -1) {
-    blockquoteTags[i].setAttribute("class", "CAUTION");
-    var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!CAUTION]", "<strong>Caution</strong><br>");
-    blockquoteTags[i].innerHTML = addBreak;
-  }
-  if (blockquoteTags[i].innerHTML.indexOf("[!WARNING]") > -1) {
-    blockquoteTags[i].setAttribute("class", "WARNING");
-    var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!WARNING]", "<strong>Warning</strong><br>");
-    blockquoteTags[i].innerHTML = addBreak;
-  }
-}
+});
