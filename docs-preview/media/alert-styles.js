759d9987ae6abbc88cce22bbb26e2f79fc204985
bharney <bharney0@gmail.com>
2019-05-03 15:20:06 -0700

    Updated styles to look more like the docs output.

--- docs-preview/media/alert-styles.js
+++ docs-preview/media/alert-styles.js
@@ -4,31 +4,31 @@ for (var i = 0; i < blockquoteTags.length; i++) {
   if (blockquoteTags[i].innerHTML.indexOf("[!NOTE]") > -1) {
     blockquoteTags[i].setAttribute("class", "NOTE");
     var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!NOTE]", "NOTE<br>");
+    var addBreak = note.replace("[!NOTE]", "<strong>Note</strong><br>");
     blockquoteTags[i].innerHTML = addBreak;
   }
   if (blockquoteTags[i].innerHTML.indexOf("[!TIP]") > -1) {
     blockquoteTags[i].setAttribute("class", "TIP");
     var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!TIP]", "TIP<br>");
+    var addBreak = note.replace("[!TIP]", "<strong>Tip</strong><br>");
     blockquoteTags[i].innerHTML = addBreak;
   }
   if (blockquoteTags[i].innerHTML.indexOf("[!IMPORTANT]") > -1) {
     blockquoteTags[i].setAttribute("class", "IMPORTANT");
     var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!IMPORTANT]", "IMPORTANT<br>");
+    var addBreak = note.replace("[!IMPORTANT]", "<strong>Important</strong><br>");
     blockquoteTags[i].innerHTML = addBreak;
   }
   if (blockquoteTags[i].innerHTML.indexOf("[!CAUTION]") > -1) {
     blockquoteTags[i].setAttribute("class", "CAUTION");
     var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!CAUTION]", "CAUTION<br>");
+    var addBreak = note.replace("[!CAUTION]", "<strong>Caution</strong><br>");
     blockquoteTags[i].innerHTML = addBreak;
   }
   if (blockquoteTags[i].innerHTML.indexOf("[!WARNING]") > -1) {
     blockquoteTags[i].setAttribute("class", "WARNING");
     var note = blockquoteTags[i].innerHTML;
-    var addBreak = note.replace("[!WARNING]", "WARNING<br>");
+    var addBreak = note.replace("[!WARNING]", "<strong>Warning</strong><br>");
     blockquoteTags[i].innerHTML = addBreak;
   }
 }
