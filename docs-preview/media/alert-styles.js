d3b4ebb379deb8eb9b853b69ebc3d8319e862106
adunndevster <adsbg6@gmail.com>
2019-06-27 09:50:40 -0600

    Added no-loc support for preview.

--- docs-preview/media/alert-styles.js
+++ docs-preview/media/alert-styles.js
@@ -12,3 +12,12 @@ alerts.map(aa => {
     }
   }
 });
+
+const noLocRegex = /:::no-loc\stext=\"([a-zA-Z'-]*)\":::/gm;
+const noLocFrontRegex = /:::no-loc\stext=\"/;
+const noLocBackRegex = /":::/;
+var noLocMatches = document.body.innerHTML.match(noLocRegex);
+noLocMatches.map(match => {
+  const noLocText = match.replace(noLocFrontRegex, "").replace(noLocBackRegex, "");
+  document.body.innerHTML = document.body.innerHTML.replace(match, noLocText);
+});
