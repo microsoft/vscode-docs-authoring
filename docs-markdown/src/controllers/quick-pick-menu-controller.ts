d0130fe9a99ac780d7f641b39fc7de68b6ad9173
bharney <bharney0@gmail.com>
2019-07-03 10:17:44 -0700

    removed xref from menus.

--- docs-markdown/src/controllers/quick-pick-menu-controller.ts
+++ docs-markdown/src/controllers/quick-pick-menu-controller.ts
@@ -16,7 +16,7 @@ import { insertSnippet } from "./snippet-controller";
 import { insertTable } from "./table-controller";
 import { applyTemplate } from "./template-controller";
 import { insertTocEntry, insertTocEntryWithOptions } from "./yaml-controller";
-import { applyXref } from "./xref-controller";
+// import { applyXref } from "./xref-controller";
 import { noLocText } from "./no-loc-controller";
 
 export function quickPickMenuCommand() {
@@ -82,10 +82,10 @@ export function markdownQuickPick() {
             description: "",
             label: "$(link) Link to heading",
         },
-        {
-            description: "",
-            label: "$(x) Link to XREF",
-        },
+        // {
+        //     description: "",
+        //     label: "$(x) Link to XREF",
+        // },
         {
             description: "",
             label: "$(lock) Non-localizable text",
@@ -215,9 +215,9 @@ export function markdownQuickPick() {
             case "cleanup...":
                 applyCleanup();
                 break;
-            case "link to xref":
-                applyXref();
-                break;
+            // case "link to xref":
+            //     applyXref();
+            //     break;
             case "toc entry":
                 insertTocEntry();
                 break;
