c097d066104beea5ff7738d2d160ab6a3bab5bcc
bharney <bharney0@gmail.com>
2019-07-09 15:32:26 -0700

    Added xref linting rules.

--- docs-markdown/markdownlint-custom-rules/xref.js
+++ docs-markdown/markdownlint-custom-rules/xref.js
@@ -16,32 +16,66 @@ module.exports = {
             inline.children.filter(function filterChild(child) {
                 return child.type === "text";
             }).forEach(function forChild(text) {
-                const textBlock = text.content;
-                const xrefMatches = textBlock.match(common.syntaxNoLocLooseMatch);
+                const textBlock = text.line;
+                const xrefMatches = textBlock.match(common.openXref);
 
                 if (xrefMatches === null) { return; }
 
                 xrefMatches.forEach(content => {
                     // no colon for "<xref:...>"
+                    if (content.match(common.xrefShouldIncludeColon)) {
+                        onError({
+                            lineNumber: text.lineNumber,
+                            detail: detailStrings.xrefShouldIncludeColon,
+                            context: text.line
+                        });
+                    }
+
+                    //xref should not have a space before the uid <xref: ...> vs <xref:...>
+                    if (content.match(common.xrefHasSpace)) {
+                        onError({
+                            lineNumber: text.lineNumber,
+                            detail: detailStrings.xrefHasSpace,
+                            context: text.line
+                        });
+                    }
+
+                    // Condition: After "<xref:...>" is missing uid.
+                    if (content.match(common.missingUidAttributeXref)) {
+                        onError({
+                            lineNumber: text.lineNumber,
+                            detail: detailStrings.missingUidAttributeXref,
+                            context: text.line
+                        });
+                    }
+
+                    // Condition: "<xref:...?displayProperty=" bad displayProperty indicator.
+                    if (content.match(common.xrefHasDisplayPropertyQuestionMark)
+                        && !content.match(common.syntaxXref)
+                        && !content.match(common.usesCorrectXrefDisplayProperties)) {
+                        onError({
+                            lineNumber: text.lineNumber,
+                            detail: detailStrings.xrefHasDisplayPropertyQuestionMark,
+                            context: text.line
+                        });
+                    }
+
+                    // Condition: "<xref:...?displayProperty=fullName|nameWithType>" bad displayProperty value.
+                    if (content.match(common.usesCorrectXrefDisplayProperties)) {
+                        onError({
+                            lineNumber: text.lineNumber,
+                            detail: detailStrings.usesCorrectXrefDisplayProperties,
+                            context: text.line
+                        });
+                    }
+
+                    // Condition: After "<xref:...>" syntax is incorrect.
                     if (!content.match(common.syntaxXref)) {
                         onError({
                             lineNumber: text.lineNumber,
                             detail: detailStrings.xrefSyntax,
                             context: text.line
                         });
-                    }
-
-                    // Condition: After "<xref:...>" syntax is incorrect.
-                    if (content.match(common.openXref) && !content.match(common.syntaxXref)) {
-                        //different errors
-                        //missing uid attribute
-                        if (!content.match(common.missingUidAttributeXref)) {
-                            onError({
-                                lineNumber: text.lineNumber,
-                                detail: detailStrings.xrefSyntax,
-                                context: text.line
-                            });
-                        }
                     }
                 });
             });
