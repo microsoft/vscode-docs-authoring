c3ecbff5278bb7b236d9c7aae55afb74322dfc8c
adunndevster <adsbg6@gmail.com>
2019-07-09 11:19:37 -0700

    Slicker resolution of xref.

--- docs-preview/src/xref/xref.ts
+++ docs-preview/src/xref/xref.ts
@@ -5,18 +5,17 @@ import { window, Position, Range } from "vscode";
 export const output = window.createOutputChannel("docs-preview");
 
 const apiUrl = "https://xref.docs.microsoft.com/query?uid=";
-const XREF_RE = /<xref:([A-Za-z_.\-\*\(\)\,\%0-9]+)(\?displayProperty=.+)?>/i;
+//Sadly because of the bug with global regexes, I need two versions. One with global, one without. - Adam
+const XREF_RE_WITH_GLOBAL = /<xref:([A-Za-z_.\-\*\(\)\,\%0-9\`}{\[\]]+)(\?displayProperty=(fullName|nameWithType))?>/gmi;
+const XREF_RE = /<xref:([A-Za-z_.\-\*\(\)\,\%0-9\`}{\[\]]+)(\?displayProperty=(fullName|nameWithType))?>/mi;
 let xrefContent = "";
 
 export function xref(md) {
   const xref = (state) => {
     try {
       // var xrefMatches = state.src.match(XREF_RE);
-      if (xrefContent === "") {
-        updateXrefContent(md, state.src);
-      } else {
-        state.src = xrefContent;
-      }
+      updateXrefContent(md, state.src);
+      state.src = xrefContent;
     } catch (error) {
       output.appendLine(error);
     }
@@ -25,10 +24,31 @@ export function xref(md) {
   md.core.ruler.before("normalize", "xref", xref);
 }
 
+let xrefMap = new Map();
 async function updateXrefContent(md: any, src: string) {
   xrefContent = src;
   let mdSrc = "";
   let captureGroup;
+
+  // check for matches, to see if there are any outside of our map.
+  const matches = src.match(XREF_RE_WITH_GLOBAL);
+  const perfectMatch = matches.every((match) => {
+    if (XREF_RE.test(match)) {
+      return xrefMap.has(match);
+    } else {
+      return true;
+    }
+  });
+
+  if (perfectMatch) {
+    while ((captureGroup = XREF_RE.exec(src))) {
+      mdSrc = xrefMap.get(captureGroup[0]);
+      src = src.slice(0, captureGroup.index) + mdSrc + src.slice(captureGroup.index + captureGroup[0].length, src.length);
+    }
+    xrefContent = src;
+    return;
+  }
+
   while ((captureGroup = XREF_RE.exec(src))) {
     // const uid = decodeSpecialCharacters(captureGroup[1].trim());
     const uid = captureGroup[1].trim();
@@ -38,6 +58,7 @@ async function updateXrefContent(md: any, src: string) {
           let xref = response.data[0];
           mdSrc = `[${xref.fullName}](${xref.href})`;
           src = src.slice(0, captureGroup.index) + mdSrc + src.slice(captureGroup.index + captureGroup[0].length, src.length);
+          xrefMap.set(captureGroup[0], mdSrc);
         }
       });
   }
