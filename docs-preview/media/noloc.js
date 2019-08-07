a180844d80b6a8dcdb56b9081e2a88c6f847e7d2
bharney <bharney0@gmail.com>
2019-08-07 15:30:51 -0700

    Added wrapper div for main content.

--- docs-preview/media/noloc.js
+++ docs-preview/media/noloc.js
@@ -2,7 +2,9 @@ const noLocRegex = /:::no-loc\stext=\"([a-zA-Z'-\s]*)\":::/gm;
 const noLocFrontRegex = /:::no-loc\stext=\"/;
 const noLocBackRegex = /":::/;
 var noLocMatches = document.body.innerHTML.match(noLocRegex);
-noLocMatches.map(match => {
-  const noLocText = match.replace(noLocFrontRegex, "").replace(noLocBackRegex, "");
-  document.body.innerHTML = document.body.innerHTML.replace(match, noLocText);
-});
+if (noLocMatches) {
+  noLocMatches.map(match => {
+    const noLocText = match.replace(noLocFrontRegex, "").replace(noLocBackRegex, "");
+    document.body.innerHTML = document.body.innerHTML.replace(match, noLocText);
+  });
+}
