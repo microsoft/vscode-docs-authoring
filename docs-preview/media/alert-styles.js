e9583413f63835c28b13cc04a96d81ea449df2ae
Adam Dunn <adsbg6@gmail.com>
2019-08-13 14:28:21 -0600

    Added icons to alerts.

--- docs-preview/media/alert-styles.js
+++ docs-preview/media/alert-styles.js
@@ -1,13 +1,33 @@
 var blockquoteTags = document.getElementsByTagName("blockquote");
-var alerts = ["Note", "Tip", "Important", "Caution", "Warning"];
+var alerts = [
+  {
+    "name": "Note",
+    "icon": "docon-status-error-outline"
+  },
+  {
+    "name": "Tip",
+    "icon": "docon-lightbulb"
+  },
+  {
+    "name": "Important",
+    "icon": "docon-status-info-outline"
+  },
+  {
+    "name": "Caution",
+    "icon": "docon-status-failure-outline"
+  },
+  {
+    "name": "Warning",
+    "icon": "docon-status-warning-outline"
+  }];
 
 alerts.map(aa => {
   for (var i = 0; i < blockquoteTags.length; i++) {
     let tag = blockquoteTags[i];
-    if (tag.innerHTML.toUpperCase().indexOf(`[!${aa.toUpperCase()}]`) > -1) {
-      tag.setAttribute("class", aa.toUpperCase());
+    if (tag.innerHTML.toUpperCase().indexOf(`[!${aa.name.toUpperCase()}]`) > -1) {
+      tag.setAttribute("class", `${aa.name.toUpperCase()}`);
       var alertType = blockquoteTags[i].innerHTML;
-      var addBreak = alertType.replace(`[!${aa.toUpperCase()}]`, `<strong>${aa}</strong><br/><hr/>`);
+      var addBreak = alertType.replace(`[!${aa.name.toUpperCase()}]`, `<strong><span class="docon ${aa.icon}"></span> ${aa.name}</strong><br/><hr/>`);
       blockquoteTags[i].innerHTML = addBreak;
     }
   }
