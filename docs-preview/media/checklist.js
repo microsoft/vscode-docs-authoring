c35563a3cd02f543030151a663bafcf1f46eea71
bharney <bharney0@gmail.com>
2019-08-23 11:03:49 -0700

    Removed blockquote around mx-tdBreakAll.

--- docs-preview/media/checklist.js
+++ docs-preview/media/checklist.js
@@ -1,6 +1,5 @@
-var checklists = document.getElementsByClassName("checklist");
+var checklists = document.querySelectorAll(".checklist");
 for (var i = 0; i < checklists.length; i++) {
     var blockquoteChecklist = upTo(checklists[i], "blockquote");
-    blockquoteChecklist.innerHTML = checklists[i].outerHTML;
     blockquoteChecklist.outerHTML = checklists[i].outerHTML;
 }
