88bfaed23383f4766d60fce1262a10f01efb7b79
bharney <bharney0@gmail.com>
2019-08-23 13:58:28 -0700

    Added conditional to check if h1 exists.

--- docs-preview/media/in-this-article.js
+++ docs-preview/media/in-this-article.js
@@ -3,21 +3,25 @@ function insertAfter(el, referenceNode) {
 }
 
 var h1 = document.querySelectorAll("h1");
-var h2 = document.querySelectorAll("h2");
-var inThisArticle = document.createElement("nav");
-inThisArticle.classList.add("doc-outline")
-var inThisArticleHeader = document.createElement("h3")
-inThisArticleHeader.innerText = "In this article";
-inThisArticle.appendChild(inThisArticleHeader)
-var articleList = document.createElement("ol")
-inThisArticle.appendChild(articleList)
-for (let index = 0; index < h2.length; index++) {
-  let element = document.createElement("li")
-  let anchor = document.createElement("a");
-  anchor.href = "#" + h2[index].innerText.replace(" ", "-");
-  anchor.innerText = h2[index].innerText;
-  element.appendChild(anchor);
-  articleList.appendChild(element)
+if (h1) {
+  var h2 = document.querySelectorAll("h2");
+  if (h2) {
+    var inThisArticle = document.createElement("nav");
+    inThisArticle.classList.add("doc-outline")
+    var inThisArticleHeader = document.createElement("h3")
+    inThisArticleHeader.innerText = "In this article";
+    inThisArticle.appendChild(inThisArticleHeader)
+    var articleList = document.createElement("ol")
+    inThisArticle.appendChild(articleList)
+    for (let index = 0; index < h2.length; index++) {
+      let element = document.createElement("li")
+      let anchor = document.createElement("a");
+      anchor.href = "#" + h2[index].innerText.replace(" ", "-");
+      anchor.innerText = h2[index].innerText;
+      element.appendChild(anchor);
+      articleList.appendChild(element)
+    }
+    inThisArticle.appendChild(articleList)
+    insertAfter(inThisArticle, h1[0])
+  }
 }
-inThisArticle.appendChild(articleList)
-insertAfter(inThisArticle, h1[0])
