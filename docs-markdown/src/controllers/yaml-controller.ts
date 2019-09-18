cd5eff114728181e005a836fee7715f47d860f2b
Jamar Warren <jamarw@users.noreply.github.com>
2019-09-18 14:31:49 -0700

    Support indented parent nodes

--- docs-markdown/src/controllers/yaml-controller.ts
+++ docs-markdown/src/controllers/yaml-controller.ts
@@ -294,6 +294,35 @@ export function createParentNode() {
   }
   const position = editor.selection.active;
   const cursorPosition = position.character;
+  const currentLine = position.line;
+  const nameScalar = /^\s+(-\sname:)/;
+  let nameIndex: boolean = false;
+  const attributeSpace = " ";
+
+  if (currentLine > 0) {
+    const startPosition = editor.selection.active.line;
+    let startingCursorPosition: number;
+    const totalLines = editor.document.lineCount;
+    let i = startPosition;
+    for (i = startPosition; i < totalLines; i--) {
+      startingCursorPosition = editor.selection.active.character;
+      if (i === 0) {
+        break;
+      }
+      const lineData = editor.document.lineAt(i);
+      const lineText = lineData.text;
+      if (lineText.match(nameScalar)) {
+        const nameScalarPosition = lineData.firstNonWhitespaceCharacterIndex;
+        if (nameScalarPosition === startingCursorPosition) {
+          nameIndex = true;
+          break;
+        } else {
+          nameIndex = false;
+          continue;
+        }
+      }
+    }
+  }
 
   if (cursorPosition === 0) {
     const parentNodeLineStart = `- name:
@@ -301,7 +330,18 @@ export function createParentNode() {
   - name:
     href:`
     insertContentToEditor(editor, insertTocEntry.name, parentNodeLineStart);
-  } else {
+  }
+
+  if (nameIndex && cursorPosition > 0) {
+    const parentNodeLineStart =
+      `- name:
+    ${attributeSpace.repeat(cursorPosition - 2)}items:
+    ${attributeSpace.repeat(cursorPosition)}- name:
+    ${attributeSpace.repeat(cursorPosition + 2)}href:`
+    insertContentToEditor(editor, insertTocEntry.name, parentNodeLineStart);
+  }
+
+  if (!nameIndex && cursorPosition !== 0) {
     window.showErrorMessage(invalidTocEntryPosition);
     return;
   }
