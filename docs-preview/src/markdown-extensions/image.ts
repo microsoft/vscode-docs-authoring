6faf973f8b4dc985782e986a825c660384735c79
Brian Harney (Excel) <brharney@microsoft.com>
2019-09-24 15:29:17 -0700

    Fixed bug in regex for icon images.

--- docs-preview/src/markdown-extensions/image.ts
+++ docs-preview/src/markdown-extensions/image.ts
@@ -6,11 +6,11 @@ export const output = window.createOutputChannel("docs-preview");
 export const imageOptions = {
     marker: ":",
     validate(params) {
-        return params.trim().match(/image(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"\s(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?:::/g)
+        return params.trim().match(/image(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"\s(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?((type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?)?:::/g)
             || params.trim().match(/image-end:::/g)
     },
     render(tokens, idx) {
-        const RE = /image(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"\s(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?:::/gm;
+        const RE = /image(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"\s(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?((type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?)?:::/gm;
         const start = RE.exec(tokens[idx].info.trim());
         if (start) {
             const source = start[start.indexOf("source") + 1]
@@ -26,6 +26,7 @@ export const imageOptions = {
 
 // Sets the type complex to each of the positions type can be in. For example, type, source, alt-text vs source, type, alt-text
 export const IMAGE_END_RE = /(?<=:::image(\s)?(type|source|alt-text)="complex"\s(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?:::)[^]+?:::image-end:::|(?<=:::image(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)"\s(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?(type|source|alt-text)="complex"(\s)?:::)[^]+?:::image-end:::|(?<=:::image(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)"\s(type|source|alt-text)="complex"(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?:::)[^]+?:::image-end:::/gm;
+export const IMAGE_ALL_RE = /:::image(\s)?(type|source|alt-text)="complex"\s(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?:::[^]+?:::image-end:::|:::image(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)"\s(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?(type|source|alt-text)="complex"(\s)?:::[^]+?:::image-end:::|:::image(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)"\s(type|source|alt-text)="complex"(\s)?(type|source|alt-text)="([a-zA-Z0-9_.\/ -]+)?"(\s)?:::[^]+?:::image-end:::|((.:*)(.\s*)(image)(.\s*)(.*)(.:*))/gm
 export function image_end(md) {
     const replaceImageEnd = (src: string) => {
         let captureGroup;
