f1bda661e778c8bdf2ec73e2a448655b18287f0d
Jamar Warren <jamarw@users.noreply.github.com>
2020-02-04 12:24:50 -0800

    Remove blank line regex and replace

--- docs-markdown/src/helper/cleanup.ts
+++ docs-markdown/src/helper/cleanup.ts
@@ -2,21 +2,18 @@
 
 export function deleteEmptyMetadata(data: any) {
     const metadataRegex: any = new RegExp(/^(\w+\.*\w+?:)(\s*|\s""|\s'')[\n|\r](?=(.|\n|\r)*---\s$)/gmi);
-    const emptyLinesRegex: any = new RegExp(/---^\s*[\n\r](?=(.|\n|\r)*---\s$)/gmi);
-    data = data.replace(metadataRegex, '').replace(emptyLinesRegex, '');
+    data = data.replace(metadataRegex, '');
     return data;
 }
 
 export function deleteNaMetadata(data: any) {
     const metadataRegex: any = new RegExp(/^(\w+\.*\w+?:\s(na|n\/a))[\n|\r](?=(.|\n|\r)*---\s$)/gmi);
-    const emptyLinesRegex: any = new RegExp(/---^\s*[\n\r](?=(.|\n|\r)*---\s$)/gmi);
-    data = data.replace(metadataRegex, '').replace(emptyLinesRegex, '');
+    data = data.replace(metadataRegex, '');
     return data;
 }
 
 export function deleteCommentedMetadata(data: any) {
-    const metadataRegex: any = new RegExp(/^(#\s?\w+\.*\w+?:).*[\n|\r](?=(.|\n|\r)*---\s$)/gmi);
-    const emptyLinesRegex: any = new RegExp(/---^\s*[\n\r](?=(.|\n|\r)*---\s$)/gmi);
-    data = data.replace(metadataRegex, '').replace(emptyLinesRegex, '');
+    const metadataRegex: any = new RegExp(/^(#\s?\w+\.*.*\w+?:).*[\n|\r](?=(.|\n|\r)*---\s$)/gmi);
+    data = data.replace(metadataRegex, '');
     return data;
 }
