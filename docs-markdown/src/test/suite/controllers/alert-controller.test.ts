19ca2f4a5ced331417733ca046ff664c4639638b
adunndevster <adsbg6@gmail.com>
2020-03-11 10:25:12 -0600

    Mocha tests work... but coverage is being a pain.

---
+++ docs-markdown/src/test/suite/controllers/alert-controller.test.ts
@@ -0,0 +1,34 @@
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+//import { insertAlert, insertAlertCommand } from '../../../controllers/alert-controller';
+//import { isMarkdownFileCheck } from '../../../helper/common';
+// import * as myExtension from '../extension';
+
+suite('Alert Controller Tests', () => {
+
+    // test('Should call insertAlertCommand', () => {
+    //     const commands = [
+    //         { command: insertAlert.name, callback: insertAlert },
+    //     ];
+
+    //     expect(commands).toEqual(insertAlertCommand());
+    // });
+    // test('isMarkdownFileCheck', () => {
+    //     insertAlert();
+    //     expect(isMarkdownFileCheck).toHaveBeenCalled();
+    // });
+});
+
+// describe("Alert Controller", () => {
+//     it("Should call insertAlertCommand.", () => {
+// const commands = [
+//     { command: insertAlert.name, callback: insertAlert },
+// ];
+
+// expect(commands).toEqual(insertAlertCommand());
+//     });
+//     it("should call isMarkdownFileCheck", () => {
+//         insertAlert();
+//         expect(isMarkdownFileCheck).toHaveBeenCalled();
+//     });
+// });
---
+++ docs-markdown/src/test/suite/index.ts
@@ -0,0 +1,78 @@
+import * as path from 'path';
+import * as Mocha from 'mocha';
+import * as glob from 'glob';
+
+export function run(): Promise<void> {
+
+
+	const NYC = require('nyc');
+
+	// create an nyc instance, config here is the same as your package.json
+	const nyc = new NYC({
+		cwd: path.join(__dirname, '..', '..', '..'),
+		include: [
+			"src/**/*.test"
+		],
+		exclude: [
+			"out/src/test/**/*.js"
+		],
+		reporter: ['text', 'html'],
+		all: true,
+		instrument: true,
+		hookRequire: true,
+		hookRunInContext: true,
+		hookRunInThisContext: true,
+		extension: [
+			".ts",
+			".tsx"
+		],
+		require: [
+			"ts-node/register"
+		],
+		sourceMap: true
+	});
+
+	nyc.reset();
+	nyc.wrap();
+
+
+	// Create the mocha test
+	const mocha = new Mocha({
+		ui: 'tdd',
+	});
+	mocha.useColors(true);
+
+	const testsRoot = path.resolve(__dirname, '..');
+
+	return new Promise((c, e) => {
+		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+			if (err) {
+				return e(err);
+			}
+
+			// Add files to the test suite
+			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+			try {
+				// Run the mocha test
+				mocha.run(failures => {
+					if (failures > 0) {
+						e(new Error(`${failures} tests failed.`));
+					} else {
+						c();
+					}
+				});
+			} catch (err) {
+				console.error(err);
+				e(err);
+			} finally {
+				if (nyc) {
+					nyc.writeCoverageFile();
+					nyc.report();
+				}
+			}
+		});
+	});
+
+
+}
