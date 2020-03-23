c2c8bfffec2503cc938fbc0c8f28b77a4f49a3b3
bharney0 <brianharney0@gmail.com>
2020-03-23 10:00:16 -0700

    Fixed mocha test coverage report.

--- docs-markdown/src/test/suite/index-coverage.ts
+++ docs-markdown/src/test/suite/index-coverage.ts
@@ -1,8 +1,39 @@
 import * as glob from "glob";
 import * as Mocha from "mocha";
 import * as path from "path";
+import { output } from "../../helper/output";
+
+function setupNyc() {
+	const NYC = require("nyc");
+	// create an nyc instance, config here is the same as your package.json
+	const nyc = new NYC({
+		cache: false,
+		cwd: path.join(__dirname, "..", "..", ".."),
+		exclude: [
+			"**/**.test.js",
+		],
+		extension: [
+			".ts",
+			".tsx",
+		],
+		hookRequire: true,
+		hookRunInContext: true,
+		hookRunInThisContext: true,
+		instrument: true,
+		reporter: ["text", "html"],
+		require: [
+			"ts-node/register",
+		],
+		sourceMap: true,
+	});
+	nyc.reset();
+	nyc.wrap();
+	return nyc;
+}
 
 export function run(): Promise<void> {
+
+	const nyc = setupNyc();
 
 	// Create the mocha test
 	const mocha = new Mocha({
@@ -12,6 +43,7 @@ export function run(): Promise<void> {
 	});
 
 	const testsRoot = path.resolve(__dirname, "..");
+	output.appendLine(testsRoot);
 	return new Promise((c, e) => {
 		glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
 			if (err) {
@@ -34,6 +66,11 @@ export function run(): Promise<void> {
 				// tslint:disable-next-line: no-console
 				console.error(err);
 				e(err);
+			} finally {
+				if (nyc) {
+					nyc.writeCoverageFile();
+					nyc.report();
+				}
 			}
 		});
 	});
