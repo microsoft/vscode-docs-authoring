c4d4864aab86fb87fbc4c68c332df76edc9631b2
Jamar Warren <jamarw@users.noreply.github.com>
2020-03-28 10:47:47 -0700

    Update snippet-controller.ts

--- docs-markdown/src/controllers/snippet-controller.ts
+++ docs-markdown/src/controllers/snippet-controller.ts
@@ -1,12 +1,13 @@
 "use strict";
 
-import * as dir from "node-dir";
-import * as vscode from "vscode";
+// import * as dir from "node-dir";
+import { subdirs } from "node-dir";
+import { resolve } from "path";
+import { QuickPickItem, window, workspace } from "vscode";
 import { hasValidWorkSpaceRootPath, isMarkdownFileCheck, isValidEditor, noActiveEditorMessage } from "../helper/common";
 import { sendTelemetryData } from "../helper/telemetry";
 import { search } from "../helper/utility";
-import { workspace } from "vscode";
-import { resolve } from "path";
+
 const fs = require("fs");
 const util = require("util");
 const readFile = util.promisify(fs.readFile);
@@ -24,7 +25,7 @@ export function insertSnippetCommand() {
  * Creates a snippet at the current cursor position.
  */
 export function insertSnippet() {
-    const editor = vscode.window.activeTextEditor;
+    const editor = window.activeTextEditor;
     if (!editor) {
         noActiveEditorMessage();
         return;
@@ -46,7 +47,7 @@ export function insertSnippet() {
 }
 // finds the directories to search, passes this and the search term to the search function.
 export function searchRepo() {
-    const editor = vscode.window.activeTextEditor;
+    const editor = window.activeTextEditor;
     let folderPath: string = "";
 
     if (!editor) {
@@ -60,13 +61,13 @@ export function searchRepo() {
 
     const selected = editor.selection;
     // There are two kinds of repo searching, whole repo, and scoped folder (both recursive)
-    const scopeOptions: vscode.QuickPickItem[] = [];
+    const scopeOptions: QuickPickItem[] = [];
 
     scopeOptions.push({ label: "Full Search", description: "Look in all directories for snippet" });
     scopeOptions.push({ label: "Scoped Search", description: "Look in specific directories for snippet" });
     scopeOptions.push({ label: "Cross-Repository Reference", description: "Reference GitHub repository" });
 
-    vscode.window.showQuickPick(scopeOptions).then(async function searchType(selection) {
+    window.showQuickPick(scopeOptions).then(async function searchType(selection) {
         if (!selection) {
             return;
         }
@@ -78,23 +79,23 @@ export function searchRepo() {
                 break;
             case "Scoped Search":
                 // gets all subdirectories to populate the scope search function.
-                dir.subdirs(folderPath, (err: any, subdirs: any) => {
+                subdirs(folderPath, (err: any, subDirs: any) => {
                     if (err) {
-                        vscode.window.showErrorMessage(err);
+                        window.showErrorMessage(err);
                         throw err;
                     }
 
-                    const dirOptions: vscode.QuickPickItem[] = [];
+                    const dirOptions: QuickPickItem[] = [];
 
-                    for (const folders in subdirs) {
-                        if (subdirs.hasOwnProperty(folders)) {
-                            dirOptions.push({ label: subdirs[folders], description: "sub directory" });
+                    for (const folders in subDirs) {
+                        if (subDirs.hasOwnProperty(folders)) {
+                            dirOptions.push({ label: subDirs[folders], description: "sub directory" });
                         }
                     }
 
-                    vscode.window.showQuickPick(dirOptions).then((directory) => {
+                    window.showQuickPick(dirOptions).then((directory) => {
                         if (directory) {
-                            search(editor, selected, directory.label)
+                            search(editor, selected, directory.label);
                         }
                     });
                 });
@@ -105,14 +106,14 @@ export function searchRepo() {
                         // get openpublishing.json at root
                         const openPublishingRepos = await getOpenPublishingFile(repoRoot);
                         if (openPublishingRepos) {
-                            const openPublishingOptions: vscode.QuickPickItem[] = [];
+                            const openPublishingOptions: QuickPickItem[] = [];
 
                             openPublishingRepos.map((repo: { path_to_root: string; url: string; }) => {
-                                openPublishingOptions.push({ label: repo.path_to_root, description: repo.url })
+                                openPublishingOptions.push({ label: repo.path_to_root, description: repo.url });
                             });
-                            vscode.window.showQuickPick(openPublishingOptions).then((repo) => {
+                            window.showQuickPick(openPublishingOptions).then((repo) => {
                                 if (repo) {
-                                    search(editor, selected, "", "", repo.label)
+                                    search(editor, selected, "", "", repo.label);
                                 }
                             });
                         }
