02b41fcaa1e12ec1d1fa08d2a78bc7e2f11e5aca
Jamar Warren <jamarw@users.noreply.github.com>
2020-04-15 15:12:23 -0700

    TSLint fixes

--- docs-metadata/src/test/extension.test.ts
+++ docs-metadata/src/test/extension.test.ts
@@ -4,7 +4,7 @@
 //
 
 // The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
+import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -12,7 +12,8 @@ import * as assert from 'assert';
 // import * as myExtension from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
+// tslint:disable only-arrow-functions
+suite("Extension Tests", function() {
 
     // Defines a Mocha unit test
     test("Something 1", function() {
