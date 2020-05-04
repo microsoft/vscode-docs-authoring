acdd0777786d1051458e9d952f9556f91ff0bbb9
bharney0 <brianharney0@gmail.com>
2020-05-04 15:13:43 -0700

    Fixed tslint errors.

--- docs-preview/src/test/suite/extension.test.ts
+++ docs-preview/src/test/suite/extension.test.ts
@@ -5,13 +5,13 @@ import { window } from "vscode";
 import { deactivate } from "../../extension";
 
 suite("Extension Test Suite", () => {
-	window.showInformationMessage("Start all tests.");
+    window.showInformationMessage("Start all tests.");
 
-	test("Sample test", () => {
-		deactivate();
+    test("Sample test", () => {
+        deactivate();
 
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
 
-	});
+    });
 });
--- docs-preview/src/test/suite/index.ts
+++ docs-preview/src/test/suite/index.ts
@@ -4,38 +4,38 @@ import * as path from "path";
 
 export function run(): Promise<void> {
 
-	// Create the mocha test
-	const mocha = new Mocha({
-		color: true,
-		timeout: 5000,
-		ui: "tdd",
-	});
+    // Create the mocha test
+    const mocha = new Mocha({
+        color: true,
+        timeout: 5000,
+        ui: "tdd",
+    });
 
-	const testsRoot = path.resolve(__dirname, "..");
-	return new Promise((c, e) => {
-		glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
+    const testsRoot = path.resolve(__dirname, "..");
+    return new Promise((c, e) => {
+        glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return e(err);
+            }
 
-			// Add files to the test suite
-			files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+            // Add files to the test suite
+            files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-			try {
-				// Run the mocha test
-				mocha.run((failures) => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				// tslint:disable-next-line: no-console
-				console.error(err);
-				e(err);
-			}
-		});
-	});
+            try {
+                // Run the mocha test
+                mocha.run((failures) => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
+                // tslint:disable-next-line: no-console
+                console.error(err);
+                e(err);
+            }
+        });
+    });
 
 }
