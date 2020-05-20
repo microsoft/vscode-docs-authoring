42a395333b52bc31d7e373c5cf7a1565d06ccab2
bharney0 <brianharney0@gmail.com>
2020-05-20 16:13:20 -0700

    Added ESLint and Prettier to docs-metadata.:

--- docs-metadata/src/util/dispose.ts
+++ docs-metadata/src/util/dispose.ts
@@ -3,13 +3,13 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 export function disposeAll(disposables: vscode.Disposable[]) {
-    while (disposables.length) {
-        const item = disposables.pop();
-        if (item) {
-            item.dispose();
-        }
-    }
+	while (disposables.length) {
+		const item = disposables.pop();
+		if (item) {
+			item.dispose();
+		}
+	}
 }
