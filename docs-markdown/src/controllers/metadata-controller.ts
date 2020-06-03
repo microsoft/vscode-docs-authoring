0c3e472ec48c50739ec6525205e88f8206820817
Matt Evans <mevansgh@gmail.com>
2020-06-02 19:57:28 -0700

    various eslint fixes

--- docs-markdown/src/controllers/metadata-controller.ts
+++ docs-markdown/src/controllers/metadata-controller.ts
@@ -5,7 +5,12 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import { commands, TextEditor, window, workspace } from 'vscode';
-import { isMarkdownFileCheck, noActiveEditorMessage, tryFindFile, toShortDate, } from '../helper/common';
+import {
+	isMarkdownFileCheck,
+	noActiveEditorMessage,
+	tryFindFile,
+	toShortDate
+} from '../helper/common';
 import { sendTelemetryData } from '../helper/telemetry';
 import { applyReplacements, findReplacement, Replacements } from '../helper/utility';
 
