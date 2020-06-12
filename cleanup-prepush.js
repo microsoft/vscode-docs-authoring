782c54a2345ba07ccfe1783a5872ddb440d948ed
bharney0 <brianharney0@gmail.com>
2020-06-12 14:04:11 -0700

    sync with origin master.

---
+++ cleanup-prepush.js
@@ -0,0 +1,19 @@
+//@ts-check
+const fs = require('fs');
+const path = require('path');
+
+const file = path.join(__dirname, './.git/hooks/pre-push');
+
+if (fs.existsSync(file)) {
+	const contents = fs.readFileSync(file);
+	if (contents.indexOf('msdocs-pre-push') !== -1) {
+		console.log('Deleting old pre-push hook.');
+		fs.unlinkSync(file);
+	} else {
+		console.log(
+			'Cleanup script determined your pre-push hook is already up to date. Nothing to do.'
+		);
+	}
+} else {
+	console.log('Found nothing here:', file);
+}
