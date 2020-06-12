0330e8fa4b2a939b863106eef8bd1e5415e92445
Jamar Warren <jamarw@users.noreply.github.com>
2020-06-12 15:40:03 -0700

    Update extension.test.ts

--- docs-markdown/src/test/suite/extension.test.ts
+++ docs-markdown/src/test/suite/extension.test.ts
@@ -1,7 +1,5 @@
-import * as assert from 'assert';
 import * as chai from 'chai';
 import * as spies from 'chai-spies';
-import { extensions, window } from 'vscode';
 import { deactivate, installedExtensionsCheck, setupAutoComplete } from '../../extension';
 import { sleep, sleepTime } from '../test.common/common';
 
@@ -9,16 +7,6 @@ chai.use(spies);
 const expect = chai.expect;
 
 suite('Extension tests', () => {
-	test('install/load', () => {
-		assert.ok(extensions.getExtension('docsmsft.docs-markdown'));
-	});
-	test('activate', () => {
-		const ext = extensions.getExtension('docsmsft.docs-markdown');
-		assert.notEqual(ext, null);
-		return ext.activate().then(() => {
-			assert.ok(true);
-		});
-	});
 	test('installedExtensionsCheck', () => {
 		const spy = chai.spy(installedExtensionsCheck);
 		installedExtensionsCheck();
