dfab2a9d6af4fa90d99cd324b94b7d678a7119c5
Jamar Warren <jamarw@users.noreply.github.com>
2020-06-12 14:01:08 -0700

    Adding tests

--- docs-markdown/src/test/suite/extension.test.ts
+++ docs-markdown/src/test/suite/extension.test.ts
@@ -1,19 +1,41 @@
 import * as assert from 'assert';
+import * as chai from 'chai';
+import * as spies from 'chai-spies';
+import { extensions, window } from 'vscode';
+import { deactivate, installedExtensionsCheck, setupAutoComplete } from '../../extension';
+import { sleep, sleepTime } from '../test.common/common';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import { window } from 'vscode';
-import { deactivate, installedExtensionsCheck } from '../../extension';
+chai.use(spies);
+const expect = chai.expect;
 
 suite('Extension Test Suite', () => {
 	window.showInformationMessage('Start all tests.');
-
-	test('Sample test', () => {
-		deactivate();
-
+	test('install/load', () => {
+		assert.ok(extensions.getExtension('docsmsft.docs-markdown'));
+	});
+	test('activate', () => {
+		const ext = extensions.getExtension('docsmsft.docs-markdown');
+		assert.notEqual(ext, null);
+		return ext.activate().then(() => {
+			assert.ok(true);
+		});
+	});
+	test('installedExtensionsCheck', () => {
+		const spy = chai.spy(installedExtensionsCheck);
 		installedExtensionsCheck();
-
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+		sleep(sleepTime);
+		expect(spy).to.be.spy;
+	});
+	test('setupAutoComplete', () => {
+		const spy = chai.spy(setupAutoComplete);
+		setupAutoComplete();
+		sleep(sleepTime);
+		expect(spy).to.be.spy;
+	});
+	test('deactivate', () => {
+		const spy = chai.spy(deactivate);
+		deactivate();
+		sleep(sleepTime);
+		expect(spy).to.be.spy;
 	});
 });
