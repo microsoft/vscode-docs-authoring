8d5a66d16fd61d1028ac2a56fdffa24d8f6df037
bharney0 <brianharney0@gmail.com>
2020-06-16 13:48:07 -0700

    Added code to just update yaml front matter.

--- docs-markdown/src/controllers/cleanup/handleMarkdownMetadata.ts
+++ docs-markdown/src/controllers/cleanup/handleMarkdownMetadata.ts
@@ -8,46 +8,46 @@ import jsyaml = require('js-yaml');
  * and convert them to single item values then return the data.
  * @param data data as yaml string from file
  */
-export function handleMarkdownMetadata(data: string, metadata: string) {
+export function handleMarkdownMetadata(metadata: string) {
 	try {
 		const yamlContent = jsyaml.load(metadata);
 		if (yamlContent) {
 			if (isSingleItemArray(yamlContent.author)) {
-				data = singleValueMetadata(data, 'author');
+				metadata = singleValueMetadata(metadata, 'author');
 			}
 			if (isSingleItemArray(yamlContent['ms.author'])) {
-				data = singleValueMetadata(data, 'ms.author');
+				metadata = singleValueMetadata(metadata, 'ms.author');
 			}
 			if (isSingleItemArray(yamlContent['ms.component'])) {
-				data = singleValueMetadata(data, 'ms.component');
+				metadata = singleValueMetadata(metadata, 'ms.component');
 			}
 			if (isSingleItemArray(yamlContent['ms.date'])) {
-				data = singleValueMetadata(data, 'ms.date');
+				metadata = singleValueMetadata(metadata, 'ms.date');
 			}
 			if (isSingleItemArray(yamlContent['ms.prod'])) {
-				data = singleValueMetadata(data, 'ms.prod');
+				metadata = singleValueMetadata(metadata, 'ms.prod');
 			}
 			if (isSingleItemArray(yamlContent['ms.service'])) {
-				data = singleValueMetadata(data, 'ms.service');
+				metadata = singleValueMetadata(metadata, 'ms.service');
 			}
 			if (isSingleItemArray(yamlContent['ms.subservice'])) {
-				data = singleValueMetadata(data, 'ms.subservice');
+				metadata = singleValueMetadata(metadata, 'ms.subservice');
 			}
 			if (isSingleItemArray(yamlContent['ms.technology'])) {
-				data = singleValueMetadata(data, 'ms.technology');
+				metadata = singleValueMetadata(metadata, 'ms.technology');
 			}
 			if (isSingleItemArray(yamlContent['ms.topic'])) {
-				data = singleValueMetadata(data, 'ms.topic');
+				metadata = singleValueMetadata(metadata, 'ms.topic');
 			}
 			if (isSingleItemArray(yamlContent['ms.title'])) {
-				data = singleValueMetadata(data, 'ms.title');
+				metadata = singleValueMetadata(metadata, 'ms.title');
 			}
 			if (isSingleItemArray(yamlContent['ms.custom'])) {
-				data = singleValueMetadata(data, 'ms.custom');
+				metadata = singleValueMetadata(metadata, 'ms.custom');
 			}
 		}
 	} catch (error) {
 		postError(error);
 	}
-	return data;
+	return metadata;
 }
