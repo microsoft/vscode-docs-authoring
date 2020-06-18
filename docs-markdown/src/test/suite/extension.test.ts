760414e434e35fef560f69fd4221e8895b1a2bbb
Jamar Warren <jamarw@users.noreply.github.com>
2020-06-18 14:39:14 -0700

    Update extension.test.ts

--- docs-markdown/src/test/suite/extension.test.ts
+++ docs-markdown/src/test/suite/extension.test.ts
@@ -6,23 +6,23 @@ import { sleep, sleepTime } from '../test.common/common';
 chai.use(spies);
 const expect = chai.expect;
 
-suite('Extension tests', () => {
+suite('Extension.ts tests', () => {
 	test('installedExtensionsCheck', () => {
 		const spy = chai.spy(installedExtensionsCheck);
 		installedExtensionsCheck();
 		sleep(sleepTime);
-		expect(spy).to.be.spy;
+		expect(spy).to.be.have.been.called;
 	});
 	test('setupAutoComplete', () => {
 		const spy = chai.spy(setupAutoComplete);
 		setupAutoComplete();
 		sleep(sleepTime);
-		expect(spy).to.be.spy;
+		expect(spy).to.be.have.been.called;
 	});
 	test('deactivate', () => {
 		const spy = chai.spy(deactivate);
 		deactivate();
 		sleep(sleepTime);
-		expect(spy).to.be.spy;
+		expect(spy).to.be.have.been.called;
 	});
 });
