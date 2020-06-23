55c663eebbfe518ab383cf04c40bbef8cd96cb3a
bharney0 <brianharney0@gmail.com>
2020-06-23 15:54:50 -0700

    Added extension test to docs-images.

---
+++ docs-images/src/test/suite/extension.test.ts
@@ -0,0 +1,58 @@
+import { activate } from '../../extension';
+import { resolve } from 'path';
+import { ExtensionContext, Uri, commands } from 'vscode';
+import chai from 'chai';
+import spies from 'chai-spies';
+chai.use(spies);
+
+const expect = chai.expect;
+
+interface Subscription {
+	dispose(): any;
+}
+interface EnvironmentalMutator {
+	type: any;
+	value: any;
+}
+const uri = resolve(__dirname, '../../../../../src/test/data/repo/articles/docs-metadata.md');
+let environmentalMutator: EnvironmentalMutator;
+let subscriptions: Subscription[] = [
+	{
+		dispose: () => {}
+	}
+];
+const context: ExtensionContext = {
+	globalState: {
+		get: (key: any) => {},
+		update: (key, value) => Promise.resolve()
+	},
+	subscriptions,
+	workspaceState: {
+		get: () => {},
+		update: (key, value) => Promise.resolve()
+	},
+	extensionPath: '',
+	asAbsolutePath: relative => '',
+	storagePath: '',
+	globalStoragePath: '',
+	logPath: '',
+	extensionUri: Uri.parse(uri),
+	environmentVariableCollection: {
+		persistent: false,
+		replace: (variable, value) => {},
+		append: (variable, value) => {},
+		prepend: (variable, value) => {},
+		get: variable => environmentalMutator,
+		forEach: () => {},
+		clear: () => {},
+		delete: () => {}
+	}
+};
+suite('Extension Tests', function () {
+	// Defines a Mocha unit test
+	test('registerCommand called', function () {
+		const spy = chai.spy.on(commands, 'registerCommand');
+		activate(context);
+		expect(spy).to.have.been.called();
+	});
+});
