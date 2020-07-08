1ac517748f6c2336acff6a132243fdfec5238c24
bharney0 <brianharney0@gmail.com>
2020-07-08 15:07:39 -0700

    Fixed indent on TOC and added unit tests.

--- docs-markdown/src/test/suite/controllers/yaml-controller.test.ts
+++ docs-markdown/src/test/suite/controllers/yaml-controller.test.ts
@@ -4,7 +4,12 @@ import { resolve, basename, dirname } from 'path';
 import { commands, window, InputBoxOptions } from 'vscode';
 import * as common from '../../../helper/common';
 import * as telemetry from '../../../helper/telemetry';
-import { loadDocumentAndGetItReady, sleep, sleepTime } from '../../test.common/common';
+import {
+	loadDocumentAndGetItReady,
+	sleep,
+	sleepTime,
+	extendedSleepTime
+} from '../../test.common/common';
 import {
 	yamlCommands,
 	insertTocEntry,
@@ -13,7 +18,8 @@ import {
 } from '../../../controllers/yaml/yaml-controller';
 import * as checkForPreviousEntry from '../../../controllers/yaml/checkForPreviousEntry';
 import * as createEntry from '../../../controllers/yaml/createEntry';
-import * as createParentNode from '../../../controllers/yaml/createParentNode';
+import * as parentNode from '../../../controllers/yaml/createParentNode';
+import { createParentNode } from '../../../controllers/yaml/createParentNode';
 import { showTOCQuickPick } from '../../../controllers/yaml/showTOCQuickPick';
 
 chai.use(spies);
@@ -34,7 +40,7 @@ suite('Yaml Controller', () => {
 		await commands.executeCommand('workbench.action.closeAllEditors');
 		sinon.restore();
 	});
-	test('yaml-controller - yamlCommands', () => {
+	test('yamlCommands', () => {
 		const controllerCommands = [
 			{ command: insertTocEntry.name, callback: insertTocEntry },
 			{
@@ -48,7 +54,7 @@ suite('Yaml Controller', () => {
 		];
 		expect(yamlCommands()).to.deep.equal(controllerCommands);
 	});
-	test('yaml-controller - insertTocEntry', async () => {
+	test('insertTocEntry', async () => {
 		const filePath = resolve(
 			__dirname,
 			'../../../../../src/test/data/repo/articles/yaml-controller.yml'
@@ -71,7 +77,7 @@ suite('Yaml Controller', () => {
 		stubShowQuickPick.restore();
 		stubShowInputBox.restore();
 	});
-	test('yaml-controller - insertTocEntryWithOptions', async () => {
+	test('insertTocEntryWithOptions', async () => {
 		const testFile = '../../../../../src/test/data/repo/articles/docs-markdown.md';
 		const testFilePath = resolve(__dirname, testFile);
 		const stubShowQuickPick = sinon.stub(window, 'showQuickPick');
@@ -89,7 +95,7 @@ suite('Yaml Controller', () => {
 		stubShowQuickPick.restore();
 		stubShowInputBox.restore();
 	});
-	test('yaml-controller - insertTocEntryWithOptions', async () => {
+	test('insertTocEntryWithOptions', async () => {
 		const testFile = '../../../../../src/test/data/repo/articles/docs-markdown.md';
 		const testFilePath = resolve(__dirname, testFile);
 		const stubShowQuickPick = sinon.stub(window, 'showQuickPick');
@@ -100,14 +106,14 @@ suite('Yaml Controller', () => {
 		const stubShowInputBox = sinon.stub(window, 'showInputBox');
 		stubShowInputBox.onCall(0).resolves('Docs Markdown Reference');
 
-		const spy = chai.spy.on(createParentNode, 'createParentNode');
+		const spy = chai.spy.on(parentNode, 'createParentNode');
 		await insertExpandableParentNode();
 		await sleep(sleepTime);
 		expect(spy).to.have.been.called();
 		stubShowQuickPick.restore();
 		stubShowInputBox.restore();
 	});
-	test('yaml-controller - showQuickPick', async () => {
+	test('showQuickPick', async () => {
 		const testFile = '../../../../../src/test/data/repo/articles/docs-markdown.md';
 		const testFilePath = resolve(__dirname, testFile);
 		const stubShowQuickPick = sinon.stub(window, 'showQuickPick');
@@ -122,6 +128,183 @@ suite('Yaml Controller', () => {
 		await showTOCQuickPick(true);
 		await sleep(sleepTime);
 		expect(spy).to.have.been.called();
+		stubShowQuickPick.restore();
+		stubShowInputBox.restore();
+	});
+	test('showQuickPick nested - formatted correctly', async () => {
+		const testFile = '../../../../../src/test/data/repo/articles/bookmark.md';
+		const testFilePath = resolve(__dirname, testFile);
+		const stubShowQuickPick = sinon.stub(window, 'showQuickPick');
+		stubShowQuickPick.onCall(0).resolves({
+			label: `${basename(testFilePath)}`,
+			description: `${dirname(testFilePath)}`
+		});
+		const stubShowInputBox = sinon.stub(window, 'showInputBox');
+		stubShowInputBox.onCall(0).resolves('This is a bookmark page');
+		const file = '../../../../../src/test/data/repo/articles/yaml-controller2.yml';
+		const filePath = resolve(__dirname, file);
+		await loadDocumentAndGetItReady(filePath);
+		const editor = window.activeTextEditor;
+		common.setCursorPosition(editor, 7, 2);
+		await showTOCQuickPick(false);
+		await sleep(sleepTime);
+		const actualText = editor?.document.getText();
+		const expectedText = [
+			'- name: Docs Markdown Reference1',
+			'  href: docs-markdown1.md',
+			'- name: template-controller.md',
+			'  items:',
+			'  - name: Docs Markdown Reference2',
+			'    href: docs-markdown2.md',
+			'  - name: This is a bookmark page',
+			'    href: bookmark.md'
+		].join('\r\n');
+		expect(expectedText).to.be.equal(actualText);
+		stubShowQuickPick.restore();
+		stubShowInputBox.restore();
+	});
+	test('showQuickPick nested with options - formatted correctly', async () => {
+		const testFile = '../../../../../src/test/data/repo/articles/bookmark.md';
+		const testFilePath = resolve(__dirname, testFile);
+		const stubShowQuickPick = sinon.stub(window, 'showQuickPick');
+		stubShowQuickPick.onCall(0).resolves({
+			label: `${basename(testFilePath)}`,
+			description: `${dirname(testFilePath)}`
+		});
+		const stubShowInputBox = sinon.stub(window, 'showInputBox');
+		stubShowInputBox.onCall(0).resolves('This is a bookmark page');
+		const file = '../../../../../src/test/data/repo/articles/yaml-controller3.yml';
+		const filePath = resolve(__dirname, file);
+		await loadDocumentAndGetItReady(filePath);
+		const editor = window.activeTextEditor;
+		common.setCursorPosition(editor, 7, 2);
+		await showTOCQuickPick(true);
+		await sleep(sleepTime);
+		const actualText = editor?.document.getText();
+		const expectedText = [
+			'- name: Docs Markdown Reference1',
+			'  href: docs-markdown1.md',
+			'- name: template-controller.md',
+			'  items:',
+			'  - name: Docs Markdown Reference2',
+			'    href: docs-markdown2.md',
+			'  - name: This is a bookmark page',
+			'    displayName: #optional string for searching TOC',
+			'    href: bookmark.md',
+			'    uid: #optional string',
+			'    expanded: #true or false, false is default'
+		].join('\r\n');
+		expect(expectedText).to.be.equal(actualText);
+		stubShowQuickPick.restore();
+		stubShowInputBox.restore();
+	});
+	test('showQuickPick first position - formatted correctly', async () => {
+		const testFile = '../../../../../src/test/data/repo/articles/bookmark.md';
+		const testFilePath = resolve(__dirname, testFile);
+		const stubShowQuickPick = sinon.stub(window, 'showQuickPick');
+		stubShowQuickPick.onCall(0).resolves({
+			label: `${basename(testFilePath)}`,
+			description: `${dirname(testFilePath)}`
+		});
+		const stubShowInputBox = sinon.stub(window, 'showInputBox');
+		stubShowInputBox.onCall(0).resolves('This is a bookmark page');
+		const file = '../../../../../src/test/data/repo/articles/yaml-controller4.yml';
+		const filePath = resolve(__dirname, file);
+		await loadDocumentAndGetItReady(filePath);
+		const editor = window.activeTextEditor;
+		common.setCursorPosition(editor, 0, 0);
+		await showTOCQuickPick(false);
+		await sleep(sleepTime);
+		const actualText = editor?.document.getText();
+		const expectedText = ['- name: This is a bookmark page', '  href: bookmark.md'].join('\r\n');
+		expect(expectedText).to.be.equal(actualText);
+		stubShowQuickPick.restore();
+		stubShowInputBox.restore();
+	});
+	test('showQuickPick first position with options - formatted correctly', async () => {
+		const testFile = '../../../../../src/test/data/repo/articles/bookmark.md';
+		const testFilePath = resolve(__dirname, testFile);
+		const stubShowQuickPick = sinon.stub(window, 'showQuickPick');
+		stubShowQuickPick.onCall(0).resolves({
+			label: `${basename(testFilePath)}`,
+			description: `${dirname(testFilePath)}`
+		});
+		const stubShowInputBox = sinon.stub(window, 'showInputBox');
+		stubShowInputBox.onCall(0).resolves('This is a bookmark page');
+		const file = '../../../../../src/test/data/repo/articles/yaml-controller5.yml';
+		const filePath = resolve(__dirname, file);
+		await loadDocumentAndGetItReady(filePath);
+		const editor = window.activeTextEditor;
+		common.setCursorPosition(editor, 0, 0);
+		await showTOCQuickPick(true);
+		await sleep(sleepTime);
+		const actualText = editor?.document.getText();
+		const expectedText = [
+			'- name: This is a bookmark page',
+			'  displayName: #optional string for searching TOC',
+			'  href: bookmark.md',
+			'  uid: #optional string',
+			'  expanded: #true or false, false is default'
+		].join('\r\n');
+		expect(expectedText).to.be.equal(actualText);
+		stubShowQuickPick.restore();
+		stubShowInputBox.restore();
+	});
+	test('createParentNode first position - formatted correctly', async () => {
+		const testFile = '../../../../../src/test/data/repo/articles/bookmark.md';
+		const testFilePath = resolve(__dirname, testFile);
+		const stubShowQuickPick = sinon.stub(window, 'showQuickPick');
+		stubShowQuickPick.onCall(0).resolves({
+			label: `${basename(testFilePath)}`,
+			description: `${dirname(testFilePath)}`
+		});
+		const stubShowInputBox = sinon.stub(window, 'showInputBox');
+		stubShowInputBox.onCall(0).resolves('This is a bookmark page');
+		const file = '../../../../../src/test/data/repo/articles/yaml-controller6.yml';
+		const filePath = resolve(__dirname, file);
+		await loadDocumentAndGetItReady(filePath);
+		const editor = window.activeTextEditor;
+		common.setCursorPosition(editor, 0, 0);
+		await createParentNode();
+		await sleep(sleepTime);
+		const actualText = editor?.document.getText();
+		const expectedText = ['- name:', '  items:', '  - name:', '    href:'].join('\r\n');
+		expect(expectedText).to.be.equal(actualText);
+		stubShowQuickPick.restore();
+		stubShowInputBox.restore();
+	});
+	test('createParentNode nested - formatted correctly', async () => {
+		const testFile = '../../../../../src/test/data/repo/articles/bookmark.md';
+		const testFilePath = resolve(__dirname, testFile);
+		const stubShowQuickPick = sinon.stub(window, 'showQuickPick');
+		stubShowQuickPick.onCall(0).resolves({
+			label: `${basename(testFilePath)}`,
+			description: `${dirname(testFilePath)}`
+		});
+		const stubShowInputBox = sinon.stub(window, 'showInputBox');
+		stubShowInputBox.onCall(0).resolves('This is a bookmark page');
+		const file = '../../../../../src/test/data/repo/articles/yaml-controller7.yml';
+		const filePath = resolve(__dirname, file);
+		await loadDocumentAndGetItReady(filePath);
+		const editor = window.activeTextEditor;
+		common.setCursorPosition(editor, 7, 2);
+		await sleep(extendedSleepTime);
+		await createParentNode();
+		await sleep(sleepTime);
+		const actualText = editor?.document.getText();
+		const expectedText = [
+			'- name: Docs Markdown Reference1',
+			'  href: docs-markdown1.md',
+			'- name: template-controller.md',
+			'  items:',
+			'  - name: Docs Markdown Reference2',
+			'    href: docs-markdown2.md',
+			'  - name:',
+			'    items:',
+			'    - name:',
+			'      href:'
+		].join('\r\n');
+		expect(expectedText).to.be.equal(actualText);
 		stubShowQuickPick.restore();
 		stubShowInputBox.restore();
 	});
