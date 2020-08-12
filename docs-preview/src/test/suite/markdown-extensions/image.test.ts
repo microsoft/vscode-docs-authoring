c4ba180321367fcbca64f53a4c624ad70449662e
bharney0 <brianharney0@gmail.com>
2020-08-12 12:32:27 -0700

    Fixed unit tests.

--- docs-preview/src/test/suite/markdown-extensions/image.test.ts
+++ docs-preview/src/test/suite/markdown-extensions/image.test.ts
@@ -1,42 +1,41 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import * as chai from 'chai';
-import { container_plugin } from '../../../markdown-extensions/container';
-import { image_end, imageOptions } from '../../../markdown-extensions/image';
+import { image_end, imageOptions, image_plugin } from '../../../markdown-extensions/image';
 const expect = chai.expect;
 
 suite('Image Extension', () => {
-	const md = require('markdown-it')().use(container_plugin, 'image', imageOptions).use(image_end);
+	const md = require('markdown-it')().use(image_plugin, 'image', imageOptions).use(image_end);
 	test('Image type content', async () => {
 		const result = md.render(
 			`:::image type="content" source="../links/media/LSIs.png" alt-text="lsi":::`
 		);
 		expect(result).to.equal(
-			`<div class="mx-imgBorder"><p><img src="../links/media/LSIs.png"></p></div>`
+			`<p><div class="mx-imgBorder"><p><img src="../links/media/LSIs.png"></p></div></p>\n`
 		);
 	});
 	test('Image type icon', async () => {
 		const result = md.render(`:::image type="icon" source="../links/media/LSIs.png":::`);
-		expect(result).to.equal(`<img src="../links/media/LSIs.png">`);
+		expect(result).to.equal(`<p><img src="../links/media/LSIs.png"></p>\n`);
 	});
 	test('Image type complex', async () => {
 		const result = md.render(`:::image type="complex" source="../links/media/LSIs.png" alt-text="lsi complex":::
         :::image-end:::`);
 		expect(result).to.equal(
-			`<div class="mx-imgBorder"><p><img src="../links/media/LSIs.png"></p></div>`
+			`<p><div class="mx-imgBorder"><p><img src="../links/media/LSIs.png"></p></div></p>\n`
 		);
 	});
 	test('Image type content - no border', async () => {
 		const result = md.render(
 			`:::image type="content" source="../links/media/LSIs.png" border="false" alt-text="lsi":::`
 		);
-		expect(result).to.equal(`<img src="../links/media/LSIs.png">`);
+		expect(result).to.equal(`<p><img src="../links/media/LSIs.png"></p>\n`);
 	});
 	test('Image type content - lightbox', async () => {
 		const result = md.render(
 			`:::image type="content" source="../links/media/LSIs.png" lightbox="../links/media/LSIs.png" alt-text="lsi":::`
 		);
 		expect(result).to.equal(
-			`<a href="../links/media/LSIs.png#lightbox" data-linktype="relative-path"><div class="mx-imgBorder"><p><img src="../links/media/LSIs.png"></p></div></a>`
+			`<p><a href="../links/media/LSIs.png#lightbox" data-linktype="relative-path"><div class="mx-imgBorder"><p><img src="../links/media/LSIs.png"></p></div></a></p>\n`
 		);
 	});
 	test('Image type content - link', async () => {
@@ -44,7 +43,7 @@ suite('Image Extension', () => {
 			`:::image type="content" source="../links/media/LSIs.png" alt-text="lsi" link="https://microsoft.com":::`
 		);
 		expect(result).to.equal(
-			`<a href="https://microsoft.com" data-linktype="relative-path"><div class="mx-imgBorder"><p><img src="../links/media/LSIs.png"></p></div></a>`
+			`<p><a href="https://microsoft.com" data-linktype="relative-path"><div class="mx-imgBorder"><p><img src="../links/media/LSIs.png"></p></div></a></p>\n`
 		);
 	});
 });
