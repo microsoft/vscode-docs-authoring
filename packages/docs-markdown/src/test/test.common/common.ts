b3075cb3a14200bb505713b80501ed4d56b53b83
JiayinPei <jipe@microsoft.com>
2020-09-29 09:58:12 +0800

    Fix CI

--- packages/docs-markdown/src/test/test.common/common.ts
+++ packages/docs-markdown/src/test/test.common/common.ts
@@ -76,5 +76,8 @@ export const context: ExtensionContext = {
 		clear: () => {},
 		delete: () => {}
 	},
-	extensionMode: ExtensionMode.Test
+	extensionMode: ExtensionMode.Test,
+	storageUri: undefined,
+	globalStorageUri: undefined,
+	logUri: undefined
 };
