d240a105b912d37f71985ab424eac171795cd446
Jamar Warren <jamarw@users.noreply.github.com>
2021-01-25 23:16:27 -0800

    Template updates

--- packages/docs-markdown-to-mail/src/controllers/mail-controller.ts
+++ packages/docs-markdown-to-mail/src/controllers/mail-controller.ts
@@ -18,13 +18,14 @@ let articleName: string;
 let authToken: string;
 let emailBody: any;
 let emailSubject: string;
-let emailBodyContent: any;
 let primaryEmailAddress: any;
 let session: any;
 let attachments = [];
 let extensionPath = extensions.getExtension('docsmsft.docs-markdown-to-mail').extensionPath;
 const alertCSS = join(extensionPath, 'media', 'alert-styles.css');
 const siteltrCSS = join(extensionPath, 'media', 'site-ltr.css');
+
+let csBody: any;
 
 export function mailerCommand() {
 	const commands = [{ command: signInPrompt.name, callback: signInPrompt }];
@@ -48,7 +49,7 @@ export async function convertMarkdownToHtml() {
 	const frontMatterRegex = /^(---)([^]+?)(---)$/gm;
 	const titleRegex = /^(#{1})[\s](.*)[\r]?[\n]/gm;
 	const h1Regex = /^#\s+/;
-	const msCustomRegex = /ms\.custom:\s?internal-blog/gm;
+	const msCustomRegex = /^ms\.custom:\s+(.*)/m;
 	const imageRegex = /src="(.*?)"/gi;
 	const editor = window.activeTextEditor;
 	let imageName: any;
@@ -63,11 +64,7 @@ export async function convertMarkdownToHtml() {
 	articleName = basename(articleName);
 	const announcementContent = window.activeTextEditor?.document.getText();
 	const metatadata = announcementContent.match(frontMatterRegex).toString();
-	let isBlog: boolean = false;
-	if (metatadata.match(msCustomRegex)) {
-		isBlog = true;
-		output.appendLine(`Blog article`);
-	}
+
 	// strip front matter to get correct title
 	let updatedAnnouncementContent = announcementContent.replace(frontMatterRegex, '');
 	const title = updatedAnnouncementContent.match(titleRegex);
@@ -115,17 +112,16 @@ export async function convertMarkdownToHtml() {
 			// removes yaml header from html
 		});
 	try {
-		emailBodyContent = md.render(updatedAnnouncementContent);
+		emailBody = md.render(updatedAnnouncementContent);
 	} catch (error) {
 		postError(error);
 		return;
 	}
 
 	// emailBody = emailBody.replace(/<h1>(.*?)<\/h1>/, '');
-	emailBody = await generateHtml(emailBodyContent, emailBodyContent);
-	console.log(emailBody);
 	output.appendLine(`Successfully coverted markdown to html.`);
 
+	csBody = emailBody;
 
 	// embed images
 	const fs = require('fs');
@@ -165,44 +161,79 @@ export async function convertMarkdownToHtml() {
 			.replace(alertText, `<p class="code-line"><strong> ${alertId}</strong><br></p><p>`);
 	}
 
-	// if the article is a blog add the banner
-	if (isBlog) {
-		const bannerLabelPath = join(extensionPath, 'images/banner-label.png').replace(/\\/g, '/');
-		const bannerFileName = basename(bannerLabelPath);
+	let bannerLabelPath: any;
+	let bannerFileName: any;
+	let bannerImagePath: any;
+	let bannerImageName: any;
+	let templateTypeMatch: any = metatadata.match(msCustomRegex);
+	let templateType = templateTypeMatch[1];
+	switch (templateType) {
+		case 'internal-blog':
+			bannerLabelPath = join(extensionPath, 'images/banner-label.png').replace(/\\/g, '/');
+			bannerFileName = basename(bannerLabelPath);
 
-		const bannerImagePath = join(extensionPath, 'images/coming-soon.png').replace(/\\/g, '/');
-		console.log(bannerImagePath);
-		const bannerImageName = basename(bannerImagePath);
-		console.log(bannerImageName);
+			bannerImagePath = join(extensionPath, 'images/banner-image.png').replace(/\\/g, '/');
+			bannerImageName = basename(bannerImagePath);
 
-		const titleImage = `
-      <table class="MsoTableGrid" border="0" cellspacing="0" cellpadding="0" style="background:#2E4B70;border-collapse:collapse;border:none"><tr style="height:3.65pt"><td width="443" valign="top" style="width:332.6pt;padding:0in 0in 0in .3in;height:3.65pt"><p class="MsoNormal"><span style="font-size:14.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:#0078D4"><img width="322" height="67" style="width:3.3541in;height:.6944in" id="Picture_x0020_3" src="cid:${bannerFileName}" alt="&quot;Content + Learning team name&quot;"></span><span style="font-size:14.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:#0078D4"><o:p></o:p></span></p></td><td width="190" rowspan="2" valign="bottom" style="width:142.5pt;padding:0in 0in 0in 0in;height:3.65pt"><p class="MsoNormal" align="right" style="text-align:right"><span style="font-size:14.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:#0078D4"><img width="190" height="158" style="width:1.9791in;height:1.6458in" id="Picture_x0020_2" src="cid:${bannerImageName}" alt="&quot;Decorative Learn Pathways image&quot;"></span><span style="font-size:14.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:#0078D4"><o:p></o:p></span></p></td></tr><tr style="height:10.75pt"><td width="443" valign="top" style="width:332.6pt;padding:0in 0in 0in .3in;height:10.75pt"><p class="MsoNormal"><span style="font-size:18.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:white">${emailSubject}<o:p></o:p></span></p></td></tr></table>`;
+			const titleImage = `
+			<table class="MsoTableGrid" border="0" cellspacing="0" cellpadding="0" style="background:#2E4B70;border-collapse:collapse;border:none"><tr style="height:3.65pt"><td width="443" valign="top" style="width:332.6pt;padding:0in 0in 0in .3in;height:3.65pt"><p class="MsoNormal"><span style="font-size:14.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:#0078D4"><img width="322" height="67" style="width:3.3541in;height:.6944in" id="Picture_x0020_3" src="cid:${bannerFileName}" alt="&quot;Content + Learning team name&quot;"></span><span style="font-size:14.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:#0078D4"><o:p></o:p></span></p></td><td width="190" rowspan="2" valign="bottom" style="width:142.5pt;padding:0in 0in 0in 0in;height:3.65pt"><p class="MsoNormal" align="right" style="text-align:right"><span style="font-size:14.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:#0078D4"><img width="190" height="158" style="width:1.9791in;height:1.6458in" id="Picture_x0020_2" src="cid:${bannerImageName}" alt="&quot;Decorative Learn Pathways image&quot;"></span><span style="font-size:14.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:#0078D4"><o:p></o:p></span></p></td></tr><tr style="height:10.75pt"><td width="443" valign="top" style="width:332.6pt;padding:0in 0in 0in .3in;height:10.75pt"><p class="MsoNormal"><span style="font-size:18.0pt;font-family:&quot;Segoe UI&quot;,sans-serif;color:white">${emailSubject}<o:p></o:p></span></p></td></tr></table>`;
+			emailBody = titleImage.concat(emailBody);
+			imageAsBase64 = fs.readFileSync(bannerLabelPath, 'base64');
+			attachments.push({
+				'@odata.type': '#microsoft.graph.fileAttachment',
+				name: bannerFileName,
+				contentType: `image/${imageExtension}`,
+				contentBytes: imageAsBase64,
+				contentId: bannerFileName,
+				isInline: true
+			});
+			imageAsBase64 = fs.readFileSync(bannerImagePath, 'base64');
+			attachments.push({
+				'@odata.type': '#microsoft.graph.fileAttachment',
+				name: bannerImageName,
+				contentType: `image/${imageExtension}`,
+				contentBytes: imageAsBase64,
+				contentId: bannerImageName,
+				isInline: true
+			});
+			break;
+		case 'docs-coming-soon':
+			try {
+				bannerImagePath = join(extensionPath, 'images/coming-soon.png').replace(/\\/g, '/');
+				bannerImageName = basename(bannerImagePath);
+				emailBody = await generateHtml(csBody, emailSubject);
+				imageAsBase64 = fs.readFileSync(bannerImagePath, 'base64');
+				attachments.push({
+					'@odata.type': '#microsoft.graph.fileAttachment',
+					name: bannerImageName,
+					contentType: `image/${imageExtension}`,
+					contentBytes: imageAsBase64,
+					contentId: bannerImageName,
+					isInline: true
+				});
+				bannerImagePath = join(extensionPath, 'images/microsoft-logo.png').replace(/\\/g, '/');
+				bannerImageName = basename(bannerImagePath);
+				//emailBody = await generateHtml(emailBodyContent, emailSubject);
+				imageAsBase64 = fs.readFileSync(bannerImagePath, 'base64');
+				attachments.push({
+					'@odata.type': '#microsoft.graph.fileAttachment',
+					name: bannerImageName,
+					contentType: `image/${imageExtension}`,
+					contentBytes: imageAsBase64,
+					contentId: bannerImageName,
+					isInline: true
+				});
+			} catch (error) {
+				console.log(error);
+			}
 
-		emailBody = titleImage.concat(emailBody);
-
-		/* 				imageAsBase64 = fs.readFileSync(bannerLabelPath, 'base64');
-						attachments.push({
-							'@odata.type': '#microsoft.graph.fileAttachment',
-							name: bannerFileName,
-							contentType: `image/${imageExtension}`,
-							contentBytes: imageAsBase64,
-							contentId: bannerFileName,
-							isInline: true
-						}); */
-		imageAsBase64 = fs.readFileSync(bannerImagePath, 'base64');
-		attachments.push({
-			'@odata.type': '#microsoft.graph.fileAttachment',
-			name: bannerImageName,
-			contentType: `image/${imageExtension}`,
-			contentBytes: imageAsBase64,
-			contentId: bannerImageName,
-			isInline: true
-		});
+			break;
 	}
 	sendMail();
 }
 
 async function sendMail() {
+
 	// Create a Graph client
 	var client = Client.init({
 		authProvider: done => {
@@ -239,38 +270,3 @@ async function sendMail() {
 	}
 }
 
-const styles = `<link rel="stylesheet" href="${siteltrCSS}">
-<link rel="stylesheet" href="${alertCSS}">
-<style>
-h2 {
-  font-size:14.0pt;
-  font-family:"Segoe UI";
-  font-weight:normal;
-  color:#0078D4;
-}
-h3 {
-  font-size:12.0pt;
-  font-family:"Segoe UI";
-  font-weight:normal;
-  color:#0078D4;
-}
-hr {
-  display: none;
-}
-p {
-  font-size:12.0pt;
-  font-family:"Segoe UI";
-}
-table {
-	width:100%;
-}
-tr {
-	height:50px;
-}
-td {
-	width:25%;
-}
-</style>
-</head>
-`;
-//}
