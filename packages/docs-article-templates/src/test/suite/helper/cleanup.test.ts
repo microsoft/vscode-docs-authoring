73e715bfc3b9da6de8c3013b74dbbf7a511a9b00
Jamar Warren <jamarw@users.noreply.github.com>
2021-03-03 07:41:53 -0800

    Update cleanup.test.ts

--- packages/docs-article-templates/src/test/suite/helper/cleanup.test.ts
+++ packages/docs-article-templates/src/test/suite/helper/cleanup.test.ts
@@ -7,7 +7,7 @@ const expect = chai.expect;
 suite('Cleanup', () => {
 	test('Cleanup downloaded files', async () => {
 		const spy = chai.spy(cleanupDownloadFiles);
-		cleanupDownloadFiles();
+		cleanupDownloadFiles(true);
 		expect(spy).to.be.have.been.called;
 	});
 });
