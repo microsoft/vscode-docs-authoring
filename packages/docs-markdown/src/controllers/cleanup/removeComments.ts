96d632a9474de885b034256169a625da5455e93d
Jamar Warren <jamarw@users.noreply.github.com>
2021-04-30 11:21:32 -0700

    Starting parser

--- packages/docs-markdown/src/controllers/cleanup/removeComments.ts
+++ packages/docs-markdown/src/controllers/cleanup/removeComments.ts
@@ -1,9 +1,16 @@
+/* eslint-disable dot-notation */
 import { reporter } from '../../helper/telemetry';
 import { readWriteFileWithProgress } from './utilities';
+
+import markdown = require('remark-parse');
+import unified = require('unified');
+import { Position, Range, TextEdit, TextEditorEdit, window } from 'vscode';
+let range;
 
 const telemetryCommand: string = 'applyCleanup';
 export const htmlRegex = new RegExp(/<!--([^]+?)-->/gm);
 export const hashtagRegex = new RegExp(/#.*/gm);
+export const codeBlockRegex = new RegExp(/`{3}([^]+?)`{3}/gm);
 
 /**
  * Delete comments from yaml and markdown
@@ -17,8 +24,9 @@ export function removeCommentsFromFile(
 	const message = 'Remove comments from file';
 	reporter.sendTelemetryEvent('command', { command: telemetryCommand });
 	if (file.endsWith('.md')) {
-		return readWriteFileWithProgress(progress, file, message, files, index, (data: string) => {
-			data = removeMarkdownComments(data);
+		return readWriteFileWithProgress(progress, file, message, files, index, (data: any) => {
+			// data = removeMarkdownComments(data);
+			data = findCodeBlocks(data);
 			return data;
 		});
 	} else if (file.endsWith('.yml')) {
@@ -28,6 +36,28 @@ export function removeCommentsFromFile(
 		});
 	} else {
 		return Promise.resolve();
+	}
+}
+
+export function findCodeBlocks(data: string) {
+	const processor: any = unified().use(markdown, { commonmark: true }).parse(data);
+	// eslint-disable-next-line no-console
+	// console.log(processor);
+	if (processor.children) {
+		processor.children.forEach(async (md: any, index: number) => {
+			try {
+				// eslint-disable-next-line no-console
+				//console.log(md['type']);
+				/* if (md['type'] === 'html') {
+					// eslint-disable-next-line no-console
+					//console.log(md['value']);
+					return;
+				} */
+			} catch (error) {
+				// eslint-disable-next-line no-console
+				console.log(error);
+			}
+		});
 	}
 }
 
