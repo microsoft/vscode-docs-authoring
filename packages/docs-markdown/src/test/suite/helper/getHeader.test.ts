74155c4528c8ce9ff19d8fa9a9dec7448c32be79
Jamar Warren <jamarw@users.noreply.github.com>
2021-06-02 10:25:41 -0700

    Update getHeader.test.ts

--- packages/docs-markdown/src/test/suite/helper/getHeader.test.ts
+++ packages/docs-markdown/src/test/suite/helper/getHeader.test.ts
@@ -7,12 +7,9 @@ const expect = chai.expect;
 
 suite('getHeadings', () => {
 	test('Get heading', async () => {
-		const filePath = resolve(
-			__dirname,
-			'../../../../../src/test/data/repo/articles/docs-markdown.md'
-		);
+		const filePath = resolve(__dirname, '../../../../../src/test/data/repo/articles/bookmark.md');
 		const content = readFileSync(filePath, 'utf8');
 		const heading = getHeadings(content);
-		expect(heading).to.equal('Docs Markdown Reference');
+		expect(heading[0]).to.equal('# This is a bookmark page');
 	});
 });
