518bf75aeb81ff9fbc72bebe6ffc73965603a204
John Papa <john@johnpapa.net>
2021-07-06 14:11:40 -0400

    color fix and const fix

--- packages/docs-visual-areas/src/models/constants.ts
+++ packages/docs-visual-areas/src/models/constants.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 
 export const extensionShortName = 'docs-visual-areas';
-export const extensionId = 'craigshoemaker.vscode-docs-visual-areas';
+export const extensionId = 'docs-msft.docs-visual-areas';
 export const visualAreasSection = 'docs-visual-areas';
 
 // export const timeout = async (ms = 200) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -14,7 +14,7 @@ export const colors = [
 	{ name: 'Blue', value: '#007fff' },
 	{ name: 'Orange', value: '#ff3d00' },
 	{ name: 'Green', value: '#42b883' },
-	{ name: 'Purple', value: '#832561' },
+	{ name: 'Yellow', value: '#F0EE22' },
 	{ name: 'Light Blue', value: '#61dafb' }
 ];
 
