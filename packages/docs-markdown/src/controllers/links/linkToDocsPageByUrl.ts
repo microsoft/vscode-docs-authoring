9eb5276c3926aed411c0ef32e05b9f78d4ffa629
bharney0 <brharney@microsoft.com>
2021-07-07 14:43:07 -0700

    CodeQL rule suppression

--- packages/docs-markdown/src/controllers/links/linkToDocsPageByUrl.ts
+++ packages/docs-markdown/src/controllers/links/linkToDocsPageByUrl.ts
@@ -22,9 +22,7 @@ const HTMLParser = require('node-html-parser');
 const telemetryCommandLink: string = 'insertLink';
 let commandOption: string;
 const allowedHosts = ['https://visualstudio.com'];
-/* lgtm[js/regex/missing-regexp-anchor] */ const regex = new RegExp(
-	/^https:\/\/docs\.microsoft\.com/
-);
+const regex = /^https:\/\/docs\.microsoft\.com/; /* lgtm[js/regex/missing-regexp-anchor] */
 export async function linkToDocsPageByUrl(urlValue?: string) {
 	commandOption = 'linkToDocsPageByUrl';
 	const editor = window.activeTextEditor;
